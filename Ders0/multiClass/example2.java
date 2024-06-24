c81ced0ab57d710081da189091e06de57ba17cf3
hackerFaruk <onuralparslan97@gmail.com>
2024-06-25 02:37:30 +0300

    creating new files with class :wq

---
+++ Ders0/multiClass/example2.java
@@ -0,0 +1,8 @@
+// sınıf adı ve dosya adı uysada sınıf public değil bu yüzden dışardan çağrılamaz 
+class example2{
+	public static void main(String[] args) {
+		System.out.println("Public classim yok");}
+	}
+public class example2public{
+	public static void main(String[] args){ 
+		System.out.println("public classtaki printim ben");}}
