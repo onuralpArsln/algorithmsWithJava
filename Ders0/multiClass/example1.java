378a0e1cc2402c6dd27202c6da2f523244da3a8b
hackerFaruk <onuralparslan97@gmail.com>
2024-06-27 13:08:50 +0300

    notes

--- Ders0/multiClass/example1.java
+++ Ders0/multiClass/example1.java
@@ -1,12 +1,17 @@
 // dosya adı ile aynı isimli bir public class
 
-public class example1{
-	public static void main(String[] args){
+public class example1 {
+	public static void main(String[] args) {
 		System.out.println("Bu class example1");
-	}}
+	}
+}
+
 // bir adet public class olacağı için bu class public değil
-class example1sideclass{
-	public static void main(String[] args){
+class example1sideclass {
+	public static void main(String[] args) {
+		// main metodun her zaman void olacak çünkü main bitince program biter
+		// program bittikten sonra bir return olmaz
 		// class public olmasa da bu sınıf public çünkü dışarıdan çağrılabilir
-	System.out.println("bu side class");}
+		System.out.println("bu side class");
+	}
 }
